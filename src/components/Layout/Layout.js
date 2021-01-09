59c2a36fbdf75065c427f68e8da36c8d1e5a83ae
Abel Toth <abeltoth7@gmail.com>
2021-01-09 23:42:38 +0100

    Static burger ui added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Aux from '../../hoc/Auxiliary/Auxiliary'
+import Aux from '../../hoc/Auxiliary/Auxiliary';
+import styles from './Layout.module.css';
 
 const layout = (props) => {
     return (
         <Aux>
             <div>Toolbar, SideDrawer, Backdrop</div>
-            <main>{props.children}</main>
+            <main className={styles.Content}>
+                {props.children}
+            </main>
         </Aux>
     )
 }
